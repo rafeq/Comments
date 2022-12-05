ec30367aa3fb4d94e6d16b07649552b60df16823
rafeq abu fiad <rafeqabufiad@rafeqs-MacBook-Pro.local>
2022-12-05 16:28:09 +0200

    first commit

--- lib/View/comment_screen.dart
+++ lib/View/comment_screen.dart
@@ -31,10 +31,10 @@ class _Comment extends State<Comment> {
     });
 
     // Add in an artificial delay
-    // await new Future.delayed(const Duration(seconds: 2));
+    await new Future.delayed(const Duration(seconds: 2));
 
-    //List.generate(20, (i) => client.getComment(i));
-    verticalData = client.getComment() as List;
+    client.getComment();
+    //verticalData = client.getComment() as List;
 
     setState(() {
       isLoadingVertical = false;
@@ -80,103 +80,4 @@ class _Comment extends State<Comment> {
       },
     );
   }
-
-/*LazyLoadScrollView(
-      isLoading: isLoadingVertical,
-      onEndOfPage: () => _loadMoreVertical(),
-      child: Scrollbar(
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: verticalData.length,
-          itemBuilder: (context, i) {
-            return ListTile(
-              leading: const Icon(Icons.person),
-              title: Text(
-                "name : ${verticalData[i].toString()}",
-                style: const TextStyle(color: Colors.cyan, fontSize: 20.0),
-              ),
-              subtitle: Text(
-                "comment : ${verticalData[i].toString()}",
-                style: const TextStyle(color: Colors.lightBlueAccent),
-              ),
-            );
-          },
-        ),
-      ),
-    ); */
-
-  /*ListView.builder(
-      controller: _scrollController,
-      itemExtent: 150,
-       itemBuilder: (context, i) {
-        print("myList ${myList[i]}");
-        return ListTile(
-            leading: const Icon(Icons.person),
-            title: Text(
-              "name : ${myList[i]["name"]}",
-              style: const TextStyle(color: Colors.cyan, fontSize: 20.0),
-            ),
-            subtitle: Text(
-              "comment : ${myList[i]["body"]}",
-              style: const TextStyle(color: Colors.lightBlueAccent),
-            ));
-      },
-    ));
-  }
-}*/
-
-/*FutureBuilder<List<Comments>>(
-        future: myList.,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final commentInfo = snapshot.data;
-            if (commentInfo != null) {
-              return ListView.builder(
-                controller: _scrollController,
-                itemExtent: 150,
-                shrinkWrap: true,
-                itemBuilder: (context, i) {
-                  return ListTile(
-                    leading: const Icon(Icons.person),
-                    title: Text(
-                      "name : ${commentInfo[i].name}",
-                      style:
-                          const TextStyle(color: Colors.cyan, fontSize: 20.0),
-                    ),
-                    subtitle: Text(
-                      "comment : ${commentInfo[i].body}",
-                      style: const TextStyle(color: Colors.lightBlueAccent),
-                    ),
-                  );
-                },
-                itemCount: commentInfo.length,
-              );
-            }
-          }
-          return const CircularProgressIndicator();
-        },
-      ), */
-
-/* return ListView.builder(
-      controller: _scrollController,
-      itemExtent: 20,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(myList[index]),
-        );
-      },
-      itemCount: 20,
-    ); */
-
-  /*Text(
-                      '${commentInfo.name} ${commentInfo.body}',
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
-                    Text(
-                      commentData.email,
-                      style: const TextStyle(fontSize: 16.0),
-                    ), 
-                    */
 }
